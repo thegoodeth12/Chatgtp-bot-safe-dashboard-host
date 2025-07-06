0d5780102c9e35b926ec8ce1d0f940f8eaf87824
thegood.eth <172343141+thegoodeth12@users.noreply.github.com>
2025-07-06 09:47:57 +0100

    Add SafeDashboard component

---
+++ app/components/SafeDashboard.tsx
@@ -0,0 +1,8 @@
+export default function SafeDashboard() {
+  return (
+    <div>
+      <p>This is where your live Safe{Wallet} info will display. 👀</p>
+      {/* Hook in dashboard-data.ts here later */}
+    </div>
+  );
+}
